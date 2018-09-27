e0c577f01e96f2653fc351fd55eaf414552fef5e
Robert <robbie123.wow@gmail.com>
2018-09-27 15:59:35 +0200

    Added EnemySpawn Scripts

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController Instance;
+
+    public GameObject enemyPrefab;
+    public float radius = 10;
+
+    public Text scoreText;
+    public int score = 0;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+
+        InvokeRepeating("Spawn", 0, 3);
+        UpdateScore();
+
+    }
+
+    void Spawn()
+    {
+        Vector3 spawnPoint = Random.insideUnitSphere * radius;
+        spawnPoint.y = 0; 
+
+        Instantiate(enemyPrefab, transform.position + spawnPoint, Quaternion.identity);
+    }
+
+    private void UpdateScore()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    public void AddScore()
+    {
+        score++;
+        UpdateScore();
+    }
+}
