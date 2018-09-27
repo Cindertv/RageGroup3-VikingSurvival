85d6fee676c351426589fa16cbaa93342efb05c1
Robert <robbie123.wow@gmail.com>
2018-09-27 16:28:09 +0200

    Adeed EnemyController Script

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+    NavMeshAgent agent;
+    public PlayerController player;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
