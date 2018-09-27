3ca1a0e22f62c8cec584c6bd99ff02244e8f853d
Robert <robbie123.wow@gmail.com>
2018-09-27 13:53:15 +0200

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 1f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector3 worldSpaceFacingDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(worldSpaceFacingDirection);
+        Vector3 direction = worldSpaceFacingDirection - transform.position;
+
+        //Get the facing angle between the player and the mouse position
+        float angle = -Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
+        Debug.Log(angle);
+
+        transform.rotation = Quaternion.Euler(-90, 90 + angle, 0);
+        float xMovement = x * movementSpeed * Time.deltaTime;
+        float yMovement = y * movementSpeed * Time.deltaTime;
+
+        transform.Translate(new Vector3(xMovement, 0, yMovement), Space.World);
+    }
+}
